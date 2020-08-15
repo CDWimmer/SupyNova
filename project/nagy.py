5a2c512ecb3f3d1c73aaae899e47f3de4f64824d
CDWimmer <31413434+CDWimmer@users.noreply.github.com>
2020-08-15 20:40:43 +0100

    Update nagy.py
    moved working code into an `if __name__ == "__main__"`, renamed wrongly named function

--- project/nagy.py
+++ project/nagy.py
@@ -1,15 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-NAGY_KAPPA15 = "../data/15M_sun.csv"
-NAGY_KAPPA30 = "../data/30M_sun.csv"
-t_k15, kappa_t15 = np.loadtxt(NAGY_KAPPA15, unpack=True, delimiter=", ")
-
-times = np.linspace(4, 365, 1000)
-
 
 def smooth_min(a, b, k):
+    """Smoothly joins two values. Shapes to min(a, b) when k>0, max when k<0"""
     a = pow(a, k); b = pow(b, k)
     return pow((a*b)/(a+b), 1/k)
 
@@ -39,10 +31,11 @@ def part2(t):
 
 
 def part3(t):
+    """Just a constant value"""
     return 0.1207 +0*t  # 0*t to work with ndarrays
 
 
-def tsp_nagy(t):
+def kappa_nagy(t):
     k1 = 30  # part1-part2 smoothing value
     k2 = -50  # part2-part3 smoothing value | negative means use maximum
     if isinstance(t, (list, np.ndarray)):
@@ -53,20 +46,27 @@ def tsp_nagy(t):
         return y
     elif isinstance(t, (int, float)):
         return smooth_min(smooth_min(part1(t), part2(t), k1), part3(t), k2)
-    # elif isinstance(t, (float, int)):
-    #     return smooth_min(smooth_min(smooth_min(part1(t), part2(t), 12), part3(t), k2))
     else:
         raise(TypeError("argument t must be a number or a list/numpy array of numbers"))
 
 
-plt.semilogx(times, part1(times), color="green", linestyle='--', label="function 1")
-plt.semilogx(times, part2(times), color="orange", linestyle='--', label="function 2")
-plt.semilogx(times, part3(times), color="purple", linestyle='--', label="function 3")
-plt.semilogx(times, tsp_nagy(times), color="red", label="Combined function")
-plt.scatter(t_k15, kappa_t15, color="blue", label="Nagy's data")
-plt.legend()
-plt.xlim(3.9,400)
-plt.ylim(-0.1,0.9)
-plt.xlabel("time (days)")
-plt.ylabel("kappa")
-plt.show()
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    NAGY_KAPPA15 = "../data/15M_sun.csv"
+    NAGY_KAPPA30 = "../data/30M_sun.csv"
+    t_k15, kappa_t15 = np.loadtxt(NAGY_KAPPA15, unpack=True, delimiter=", ")
+
+    times = np.linspace(4, 365, 1000)
+    
+    plt.semilogx(times, part1(times), color="green", linestyle='--', label="function 1")
+    plt.semilogx(times, part2(times), color="orange", linestyle='--', label="function 2")
+    plt.semilogx(times, part3(times), color="purple", linestyle='--', label="function 3")
+    plt.semilogx(times, kappa_nagy(times), color="red", label="Combined function")
+    plt.scatter(t_k15, kappa_t15, color="blue", label="Nagy's data")
+    plt.legend()
+    plt.xlim(3.9,400)
+    plt.ylim(-0.1,0.9)
+    plt.xlabel("time (days)")
+    plt.ylabel("kappa")
+    plt.show()
